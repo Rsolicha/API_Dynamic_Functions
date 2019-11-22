57c7d8687ef6256d10b2d7074afb1b245c1b3d2d
rsolicha <rsolicha@gmail.com>
2019-11-21 22:01:19 -0600

    Act filter descripcion

--- functions/Manager/function_manager.js
+++ functions/Manager/function_manager.js
@@ -7,8 +7,22 @@ const QUERY = require('../Utilities/query_call');
 async function createFunction(data) {
     let assign = new Date();
     let date = `${assign.getDate()}${assign.getSeconds()}${assign.getMilliseconds()}${Math.floor(Math.random() * 100000)}`;
+    data.keywords = await splitDescription(data.description);
     let response = await QUERY.QueryAdd('functions', date, data);
     return response !== 2 ? true : false
+}
+
+/**
+ * @function splitDescription get a text and split in list of words
+ * @param {String} word text to apply split
+ */
+async function splitDescription(word) {
+    let wordNormalized = word.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    let textlowercase = wordNormalized.toLowerCase();
+    let listWords = textlowercase.split(/[^\w]/)
+    let filter = [];
+    listWords.map(element => element !== "" ? filter.push(element) : false)
+    return filter;
 }
 
 /**
@@ -17,6 +31,7 @@ async function createFunction(data) {
  * @param {string} idFunction id function to change
  */
 async function updateFunction(data, idFunction) {
+    data.keywords = await splitDescription(data.description);
     let response = await QUERY.QueryUpdate('functions', idFunction, data);
     return response !== 2 ? true : false
 }
@@ -61,7 +76,7 @@ async function getCodesFunction(idFunction) {
     let response = await QUERY.QueryGetDocument('functions', idFunction);
     if (response) {
         if (response.functions) {
-            let associated = await verifyFunctionsAssociated(response.functions);
+            let associated = await verifyFunctionsAssociated(response.functions, idFunction);
             return response.code + '\n' + associated;
         }
         return response.code + '\n';
@@ -75,11 +90,12 @@ async function getCodesFunction(idFunction) {
  * @function verifyFunctionsAssociated verifies if function has dependencies and call them for gets all codes
  * @param {array} functions list of id functions
  */
-async function verifyFunctionsAssociated(functions) {
+async function verifyFunctionsAssociated(functions, idFunction) {
     let stringResult = "";
     await Promise.all(functions.map(async(element) => {
-        stringResult = stringResult + await getCodesFunction(element);
-
+        if (element !== idFunction) {
+            stringResult = stringResult + await getCodesFunction(element);
+        }
     }));
     return stringResult;
 }
@@ -110,13 +126,14 @@ async function getFunctionInfo(idFunction) {
 async function getSearch(username, description, code, tag, function_name) {
     let flag = 0;
     var list = [];
+    description ? list = await descriptionFilter(description) : flag++;
     username ? list.push(await QUERY.QueryRead('functions', 'user', '==', username)) : flag++;
-    description ? list.push(await QUERY.QueryRead('functions', 'description', '==', description)) : flag++;
     code ? list.push(await QUERY.QueryRead('functions', 'code', '==', code)) : flag++;
     tag ? list.push(await QUERY.QueryRead('functions', 'tag', '==', tag)) : flag++;
     function_name ? list.push(await QUERY.QueryRead('functions', 'name', '==', function_name)) : flag++;
     flag === 5 ? list.push(await QUERY.QueryCollection('functions')) : flag = 6
     return await checkData(list);
+
 }
 
 /**
@@ -156,6 +173,22 @@ async function checkData(list) {
     }));
 
     return result;
+}
+
+/**
+ * @function descriptionFilter check in database all words in text that are equal
+ * @param {String} description 
+ */
+async function descriptionFilter(description) {
+    let wordNormalized = description.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    let textlowercase = wordNormalized.toLowerCase();
+    let listWords = textlowercase.split(' ')
+    let listResults = [];
+    await Promise.all(listWords.map(async(word) => {
+        listResults.push(await QUERY.QueryRead('functions', 'keywords', 'array-contains', word))
+
+    }));
+    return listResults;
 }
 
 
